df08af1082ff9baa528fcd18fb930303fe92b43b
vnsemkin@gmail.com <vnsemkin@gmail.com>
2023-05-17 13:39:39 +0300

    17.05.2023

---
+++ src/main/java/controller/filter/AuthorizationFilter.java
@@ -0,0 +1,31 @@
+package controller.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "AuthorizationFilter", value = "/*")
+public class AuthorizationFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        String defaultHttp = "/index";
+        String url = httpRequest.getRequestURI();
+        String userName = (String) httpRequest.getSession().getAttribute("name");
+        if (userName == null && isAuthenticationUrl(url) || url.equals(defaultHttp)) {
+            httpRequest.getServletContext().getRequestDispatcher(url).forward(request, response);
+        } else if (userName != null && !isAuthenticationUrl(url)) {
+            httpRequest.getServletContext().getRequestDispatcher(url).forward(request, response);
+        } else {
+            httpResponse.sendRedirect(defaultHttp);
+        }
+    }
+
+    private boolean isAuthenticationUrl(String url) {
+        return url.equals("/login") || url.equals("/signup");
+    }
+}
---
+++ src/main/java/controller/servlet/LogoutServlet.java
@@ -0,0 +1,17 @@
+package controller.servlet;
+
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ServletLogout", value = "/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        response.sendRedirect("/index.jsp");
+    }
+}
